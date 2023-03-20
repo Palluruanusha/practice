491f8217a39d89c686aba8a606dda392b49aeb8d
Anusha Palluru <anusha.palluru@gmail.com>
2023-03-20 21:07:53 +0530

    Adding LocalVariables practice program

---
+++ Java/Variables/LocalVariables.java
@@ -0,0 +1,8 @@
+class LocalVariables 
+{
+	public static void main(String[] args) 
+	{
+		int a; //local variable
+		System.out.println(a); //compile time error because values must be intialised before using
+	}
+}
---
+++ Java/Variables/LocalVariables.java.bak
@@ -0,0 +1,8 @@
+class LocalVariables 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		System.out.println(a);
+	}
+}
