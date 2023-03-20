6a58672d0995f15c93f3b207f070c4d49ae98277
Anusha Palluru <anusha.palluru@gmail.com>
2023-03-20 20:43:28 +0530

    Adding Local Variables practice program

---
+++ Java/Variables/VariablesLocal.java
@@ -0,0 +1,12 @@
+class VariablesLocal
+{
+	int a;
+	String str;
+	VariablesLocal temp;
+	    public static void main(String[] args) { 
+		VariablesLocal variablesLocal = new VariablesLocal();
+		System.out.println(variablesLocal.a);
+		System.out.println(variablesLocal.temp);
+		System.out.println(variablesLocal.str);
+	}
+}
