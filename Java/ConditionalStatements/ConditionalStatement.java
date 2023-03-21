a429dbca710b9cb472bb43943c57d19e1cf62a2d
Anusha Palluru <anusha.palluru@gmail.com>
2023-03-21 09:53:01 +0530

    Conditional Statements practice program

---
+++ Java/ConditionalStatements/ConditionalStatement.java
@@ -0,0 +1,14 @@
+class ConditionalStatement 
+{
+	public static void main(String[] args) 
+	{
+		boolean hasGoodFriend=true;
+			if(hasGoodFriend)
+		{
+		System.out.println("Welcome to Java");
+		{
+		System.out.println("Ha Yes");
+		}
+	}
+}
+}
