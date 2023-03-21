fd3481ce4967d807f24f0482155cd7344fd37afc
Anusha Palluru <anusha.palluru@gmail.com>
2023-03-21 10:03:55 +0530

    Adding Odd number program

---
+++ Java/ConditionalStatements/Odd.java
@@ -0,0 +1,16 @@
+class Odd 
+{
+	public static void main(String[] args) 
+	{
+		int a=5;
+		if(a%2==0)
+		{
+
+		System.out.println(a+"Is Even number");
+		}
+		else
+		{
+			System.out.println(a+"Is Odd number");
+		}
+	}
+}
