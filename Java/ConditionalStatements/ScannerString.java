ae88020e658ace16fb4025e580551cdafda88a75
Anusha Palluru <anusha.palluru@gmail.com>
2023-03-21 20:56:01 +0530

    Create ScannerString.java

---
+++ Java/ConditionalStatements/ScannerString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class ScannerString
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter String");
+		String str=scn.next();
+		System.out.println("Entered String Is" +str);
+	}
+}
