7feb18de0be5e0a7f3c7112ff4e7995df0237c2e
Anusha Palluru <anusha.palluru@gmail.com>
2023-03-22 20:10:59 +0530

    Adding String practice program

---
+++ Java/ConditionalStatements/StringProgram.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class StringProgram
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter String");
+		String str=scn.nextLine();
+		System.out.println("Enter String is" +str);
+	}
+}
