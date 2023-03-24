67c71539c27f7ff36f2d1668659d1eb9886e3b2b
Anusha Palluru <anusha.palluru@gmail.com>
2023-03-24 20:56:37 +0530

    Using break condition program

---
+++ Java/LoopingStatements/Break1.java
@@ -0,0 +1,16 @@
+class Break1 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<10;i++)
+		{
+			System.out.println(i);
+		if(i==3)
+		{
+			break;
+		}
+
+		System.out.println("outside for");
+		}
+	}
+}
---
+++ Java/LoopingStatements/Continue.java
@@ -0,0 +1,16 @@
+class Continue 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<10;i++)
+		{
+			if(i%2==0)//for even number if(n%2!=0)
+			{
+				continue;
+			}
+			System.out.println(i);
+		}
+		System.out.println("outside for");
+	}
+}
+
