7d030c0efbebbd5782c652e0dcc0ca6b7d2b10da
Anusha Palluru <anusha.palluru@gmail.com>
2023-03-24 21:09:48 +0530

    using return statement practice program

---
+++ Java/LoopingStatements/Return.java
@@ -0,0 +1,15 @@
+class Return 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+			return;
+		}
+		System.out.println(i);
+	}
+		System.out.println("outside for");
+	}
+}
