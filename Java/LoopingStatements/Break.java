77fca862c343b28a49e87e2542b59f5c7bc2d06a
Anusha Palluru <anusha.palluru@gmail.com>
2023-03-24 20:39:58 +0530

    Adding program by using break

---
+++ Java/LoopingStatements/Break.java
@@ -0,0 +1,16 @@
+class Break 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=10;i++)
+		{
+			if(i==3)
+			{
+				break;
+			}
+		System.out.println(i);
+		}
+		System.out.println("outside for");
+	}
+}
+
