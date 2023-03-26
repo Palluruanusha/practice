a2babf1257b0621239b350d133c32dd20b732f5e
Anusha Palluru <anusha.palluru@gmail.com>
2023-03-26 15:11:24 +0530

    fibonacci count practice program

---
+++ Java/LoopingStatements/FibCount.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+class FibCount 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter Lower and Upper limit");
+		int n=scn.nextInt();
+		int m=scn.nextInt();
+		int a=0;
+		int b=1;
+		int c=0;
+		int count=0;
+		for(;;) 
+		{
+		c=a+b;
+		if(c>m)
+		{
+			break;
+		}
+		if(c>=n)
+		{
+			count++;
+		}
+		a=b;
+		b=c;
+	}
+	System.out.println("count is" +count);
+}
+}
