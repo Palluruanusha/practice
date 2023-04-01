c0a83d66f9a5d2a45ebead84843b4084d2fbe6aa
Anusha Palluru <anusha.palluru@gmail.com>
2023-04-01 19:17:26 +0530

    Adding nested forloop using return program

---
+++ Java/LoopingStatements/Exp1.java
@@ -0,0 +1,30 @@
+class Exp1 
+{
+	public static void main(String[] args) 
+	{
+		int i,j=0;
+		for (i=1;i<=5;i++)
+		{
+			/*if(i==j)
+			{
+			return;
+			}
+		}*/
+			
+			for(j=1;j<=5;j++)
+			{
+
+			if(i==j)
+			{
+			return;
+			}
+				System.out.println(i+" "+j);
+		}
+	}
+}
+}
+
+	
+
+
+			
