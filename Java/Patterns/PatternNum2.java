3a445655be956ddeeb9775996b2d0da3350694e3
Anusha Palluru <anusha.palluru@gmail.com>
2023-04-09 19:14:55 +0530

    Adding pattern num2 program

---
+++ Java/Patterns/PatternNum2.java
@@ -0,0 +1,17 @@
+class PatternNum2
+{
+	public static void main(String[] args) 
+	{
+		int count=0;
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				count++;
+		System.out.print(count);
+	}
+	System.out.println();
+		}
+	}
+
+}
