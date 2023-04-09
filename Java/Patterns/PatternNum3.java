d757798352bbe5f3953a02b3c0af533eda79fac1
Anusha Palluru <anusha.palluru@gmail.com>
2023-04-09 19:15:31 +0530

    Adding pattern num3 program

---
+++ Java/Patterns/PatternNum3.java
@@ -0,0 +1,27 @@
+class PatternNum3
+{
+	public static void main(String[] args) 
+	{
+		//int count=1;
+		for(int i=1; i<=5; i++)
+		{
+			if(i%2!=0)
+			{
+			for(int j=1; j<=5; j++)
+			{
+		System.out.print(j);
+	}
+			}
+	else
+				{
+		for(int j=5;j>=1;j--)
+					{
+			System.out.print(j);
+					}
+				}
+
+	System.out.println();
+		}
+	}
+
+}
