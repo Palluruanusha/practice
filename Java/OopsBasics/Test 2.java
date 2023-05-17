b6a3efe2b3dc548407ee55d21df0c5925729a164
Anusha Palluru <anusha.palluru@gmail.com>
2023-05-17 07:41:52 +0530

    adding Basic oops program

---
+++ Java/OopsBasics/Test 2.java
@@ -0,0 +1,21 @@
+class B
+{
+}
+{
+	class C
+	{
+		C(int i)
+		{
+		}
+	}
+	class Test 2
+	{
+	public static void main(String[] args) 
+	{
+		new B();
+		new B(10);
+		new C();
+		new C(10);
+		//System.out.println("Hello World!");
+	}
+}
