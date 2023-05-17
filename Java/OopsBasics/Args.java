280a3b2a5460a5a0d574dee7947f4208592d808e
Anusha Palluru <anusha.palluru@gmail.com>
2023-05-17 07:42:40 +0530

    adding basic Args program

---
+++ Java/OopsBasics/Args.java
@@ -0,0 +1,12 @@
+class Args 
+{
+	public static void main(String[]args) 
+	{
+		//int i=0;
+		//System.out.println(args[i]);
+		for(int i=0;i<args.length;i++)
+		{
+		System.out.println(args[i]);
+		}
+	}
+}
