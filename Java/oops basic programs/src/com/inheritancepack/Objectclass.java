b606bd4dad667f0dc3adbd2b9d6d368cbf3a3e60
Anusha Palluru <anusha.palluru@gmail.com>
2023-06-19 07:56:50 +0530

    Object class with default constructor id executed internally

---
+++ Java/oops basic programs/src/com/inheritancepack/Objectclass.java
@@ -0,0 +1,22 @@
+package com.inheritancepack;
+class A0
+
+{
+	public void Object()
+	{
+	}
+}
+class A1 extends Object
+{
+	//default constructor
+	
+}
+public class Objectclass {
+	public static void main(String[] args)
+	{
+		new A1();
+		System.out.println("**************");
+		
+	}
+
+}
