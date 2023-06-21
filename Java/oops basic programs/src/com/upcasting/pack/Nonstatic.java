addf5fd79036a68d6e6fc7421c8e568638536ca6
Anusha Palluru <anusha.palluru@gmail.com>
2023-06-21 19:52:06 +0530

    up casting using Static methods

---
+++ Java/oops basic programs/src/com/upcasting/pack/Nonstatic.java
@@ -0,0 +1,37 @@
+package com.upcasting.pack;
+class A
+{
+	public void m1()
+	{
+		System.out.println("M1 of A");
+	}
+}
+class B extends A
+{
+	@override
+	public void m1()
+	{
+		System.out.println("M1 of B");
+	}
+}
+class C extends B
+{
+	@override
+	public void m1()
+	{
+		System.out.println("M1 of C");
+	}
+}
+public class Nonstatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A a;
+		a=new B();
+		a.m1();
+		a=new C();
+		a.m1();
+
+	}
+
+}
