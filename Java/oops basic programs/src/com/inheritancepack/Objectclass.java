8e838d70fe2a146643af079680458c6f52ef5b1c
Anusha Palluru <anusha.palluru@gmail.com>
2023-07-17 12:43:11 +0530

    Adding Object class example practice program

--- Java/oops basic programs/src/com/inheritancepack/Objectclass.java
+++ Java/oops basic programs/src/com/inheritancepack/Objectclass.java
@@ -1,20 +1,22 @@
 package com.inheritancepack;
-class A0
-
+class Object
 {
 	public void Object()
 	{
 	}
 }
-class A1 extends Object
+class AA1 extends Object
 {
 	//default constructor
+	public AA1()
+	{
+		super();
+	}
 	
 }
 public class Objectclass {
 	public static void main(String[] args)
 	{
-		new A1();
 		System.out.println("**************");
 		
 	}
