5572212e9f46d6ed791bd3bf1b850d8c11c70c28
Anusha Palluru <anusha.palluru@gmail.com>
2023-08-09 23:25:51 +0530

    pattern program practice

---
+++ Java/oops basic programs/src/com/patterns/pack/Patt1.java
@@ -0,0 +1,18 @@
+package com.patterns.pack;
+
+public class Patt1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int size=5;
+		for(int i=1;i<=size;i++) {
+			for(int j=1;j<=size;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
+
+
