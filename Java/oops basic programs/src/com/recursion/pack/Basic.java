71ae8e1fc6b993ceeb423e674953fd8df150652f
Anusha Palluru <anusha.palluru@gmail.com>
2023-08-09 23:34:22 +0530

    Basic recursion program practice

---
+++ Java/oops basic programs/src/com/recursion/pack/Basic.java
@@ -0,0 +1,20 @@
+package com.recursion.pack;
+
+public class Basic {
+	public static void m1(String[] args) {
+		System.out.println("Hello");
+		m2();
+		System.out.println("ok bye");
+		}
+		public static void m2(String[] args) {
+			System.out.println("hey");
+			m3(); 
+			System.out.println("its ok");
+		}
+			public static void m3(String[] args) {
+				System.out.println("alteady comitted");
+		}
+}
+		
+		
+
