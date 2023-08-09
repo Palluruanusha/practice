8f258a17c5d664b1e25b9843ba82cb916627690a
Anusha Palluru <anusha.palluru@gmail.com>
2023-08-09 23:33:03 +0530

    string Program

---
+++ Java/Patterns/Patt.java
@@ -0,0 +1,23 @@
+class Patt
+{
+	public static void main(String []args)
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				if(i==j)
+				{
+				}
+				else if(i+j==5+1)
+				{
+				}
+				else{
+					System.out.println(" ");
+				}
+			}
+		}
+	}
+}
+
+				
---
+++ Java/oops basic programs/src/com/strings/pack/ILovePythJava.java
@@ -0,0 +1,12 @@
+package com.strings.pack;
+
+public class ILovePythJava {
+	public static void main(String[] args) {
+		String s1="i love python";
+		System.out.println("s1="+s1);
+		String s2 = s1.replace("python", "java");
+		            System.out.println("s2 =" + s2);
+	}
+		    
+
+}
