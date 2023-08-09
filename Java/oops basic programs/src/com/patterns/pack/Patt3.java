b1bb514d270176143ae41a8b07f4979b219292df
Anusha Palluru <anusha.palluru@gmail.com>
2023-08-09 23:28:03 +0530

    Example of pattern program

---
+++ Java/oops basic programs/src/com/patterns/pack/Patt3.java
@@ -0,0 +1,17 @@
+package com.patterns.pack;
+
+public class Patt3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int size=5;
+		for(int i=1;i<=size;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
