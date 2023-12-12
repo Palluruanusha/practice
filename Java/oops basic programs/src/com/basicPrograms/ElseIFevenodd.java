0028990052f4d51d1a81fec7be78143f08bda1a2
Anusha Palluru <anusha.palluru@gmail.com>
2023-12-12 12:51:01 +0530

    Basic Else if Example practice program

---
+++ Java/oops basic programs/src/com/basicPrograms/ElseIFevenodd.java
@@ -0,0 +1,47 @@
+package com.basicPrograms;
+
+import java.util.Scanner;
+
+public class ElseIFevenodd {
+
+	/*public static void main(String[] args) {
+		int a=5;
+		int rem=a%2;
+		boolean res=(rem==0);
+		if(res)
+			System.out.println(a+"is a even number");
+		else
+		System.out.println(a+"is not a even number");	
+	} */
+	
+	
+//even odd program using scanner input
+	/*public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		System.out.println("enter a value");
+		int n=scn.nextInt();	
+		if(n%2==0)
+		System.out.println(n+ "is a even number");
+		else
+			System.out.println(n+ "is a odd number");
+	}*/
+	
+
+	/*public static void main(String[] args) {
+		int a=4;
+		if(a%2==0)
+			System.out.println("even number");
+		else
+			System.out.println("odd number");
+	}*/
+	
+	public static void main(String []args) {
+		int a=5;
+		if(a%2==0)
+			System.out.println(a+" is a even number");
+		else
+			System.out.println(a+" is not a odd number");
+		
+	}
+
+}
